aa998122d2f49aeff40b4530a56a937c35fee2b4
Pedro Abreu <pvictor@novakio.com.br>
2023-07-23 13:25:09 -0300

    att

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'cpf' => '11111111111',
         //     'email' => 'test@example.com',
         // ]);
+        \App\Models\User::create([
+            'name' => 'Test_User1',
+            'cpf' => '99999999999',
+            'email' => 'testeteste3@example.com',
+            'password' => bcrypt('123456'),
+        ]);
     }
 }
