c0ee56da57b832f47df2d002178926341391262a
Marcus Rodrigues <marcus1751@outlook.com>
2023-07-24 21:15:41 -0300

    feat logout system

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -12,20 +12,19 @@
 
     <li class="nav-item">
         <a class="nav-link" href="#">
+            {{-- <a class="nav-link" href="{{ route('suporte') }}"> --}}
             <i class="fas fa-fw fa-envelope"></i>
             <span>Suporte</span>
         </a>
     </li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+    <li class="nav-item">
+        <a class="nav-link" href="/empenhos-emitidos">
+            <i class="fas fa-fw fa-file-invoice-dollar"></i>
+            <span>Empenhos Emitidos</span>
+        </a>
+    </li>
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Interface
-    </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
             aria-expanded="true" aria-controls="collapseTwo">
@@ -35,8 +34,8 @@
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Consultas:</h6>
-                <a class="collapse-item" href="#">Exportar</a>
-                <a class="collapse-item" href="#">Orçamento</a>
+                <a class="collapse-item" href="/exportador-contabil">Exportar</a>
+                <a class="collapse-item"  href="/orcamentario">Orçamentario</a>
             </div>
         </div>
     </li>
@@ -59,3 +58,10 @@
     </div>
 
 </ul>
+
+<style>
+    .bg-gradient-primary {
+        background-color: #035c87 !important;
+        background-image: unset;
+    }
+</style>
--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -15,12 +15,21 @@
 
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
-            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
-                <img class="img-profile rounded-circle"
-                    src="/assets/img/undraw_profile.svg">
+                <img class="img-profile rounded-circle" src="/assets/img/undraw_profile.svg">
             </a>
+            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+        
+                    <button type="submit" class="dropdown-item">
+                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                        {{ __('Log Out') }}
+                    </button>
+                </form>
+            </div>
         </li>
 
     </ul>
